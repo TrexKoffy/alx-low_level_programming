3fcd17ae923db2ce3f8c0bc3dc2a991ab8cdf9f4
Anthony Olayemi <thonyfrancis8669@gmail.com>
2022-09-08 12:33:36 -0700

    betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 /**
 * main - Entry point 
+* Description: Using the puts function for Bety
 * Return: Always 0 (Success)
 */
 int main(void)
-{ puts("\"Programming is like building a multilingual puzzle");return (0);}
+{ 
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
