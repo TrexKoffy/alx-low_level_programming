de85739ce2fd821732c386d40e39201ed4b6a846
Anthony Olayemi <thonyfrancis8669@gmail.com>
2022-09-08 11:01:50 -0700

    return

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,9 +3,8 @@
 *main - Entry point 
 *Return - Always 0 (Success)
 */
-
-int main()
-     {
-       puts("Programming is like building a multilingual puzzle\n");
+int main(void)
+{
+       puts("\"Programming is like building a multilingual puzzle\n");
        return 0;
-         }
+}
