dba3aeb629e19d45a3ec1561cd5d1582c11a5dbb
Anthony Olayemi <thonyfrancis8669@gmail.com>
2022-09-08 12:17:00 -0700

    whitespace

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,7 +4,4 @@
 * Return: Always 0 (Success)
 */
 int main(void)
-{
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
-}
+{ puts("\"Programming is like building a multilingual puzzle");return (0);}
