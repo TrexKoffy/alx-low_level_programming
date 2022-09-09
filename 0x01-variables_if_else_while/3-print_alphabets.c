0895a2965ca75f98bbecd86f0fd3735270c1270c
Anthony Olayemi <thonyfrancis8669@gmail.com>
2022-09-09 15:51:34 -0700

    messgae

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+*main - Entry point
+*Description: print small and big alphabets
+*Return: always 0
+*/
+int main(void)
+{
+char x;
+for(x = 'a'; x <= 'z'; x++)
+for(x = 'A'; x <= 'Z'; x++)
+putchar(x);
+putchar ('\n');
+return (0);
+}
