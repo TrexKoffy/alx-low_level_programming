5229794a0e09f93b3334687662fe797730c88e91
Anthony Olayemi <thonyfrancis8669@gmail.com>
2022-09-09 16:29:49 -0700

    i am coming

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+*main - Entry point
+*Description: print small alphabets
+*Return: always 0
+*/
+
+int main(void)
+{
+char x;
+for (x = 'a'; a <= 'z' ; x++)
+{
+if (x == 'q' || x == 'e')
+{
+continue;
+}
+putchar(x);
+putchar('\n');
+return (0);
+}
