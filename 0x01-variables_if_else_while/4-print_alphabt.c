92ac70351bd0acaa03ec81e2473ccee4edc5696f
Anthony Olayemi <thonyfrancis8669@gmail.com>
2022-09-09 16:34:23 -0700

    fissi

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -8,14 +8,14 @@
 
 int main(void)
 {
-char x;
-for (x = 'a'; a <= 'z' ; x++)
+char a;
+for (a = 'a'; a <= 'z' ; a++)
 {
-if (x == 'q' || x == 'e')
+if (a == 'q' || a == 'e')
 {
 continue;
 }
-putchar(x);
+putchar(a);
 putchar('\n');
 return (0);
 }
