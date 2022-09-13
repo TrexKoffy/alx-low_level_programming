8de01f2a5fa69a95074bced242edac20cb890178
Anthony Olayemi <thonyfrancis8669@gmail.com>
2022-09-13 09:56:37 -0700

    git

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,15 +4,15 @@
 * print_alphabet_x10 - Prints 10 times the alphabet in lower case
 */
 
-void print_alphabet_x10(void) 
+void print_alphabet_x10(void)
 {
 	int num = 0;
-	char small; 
+	char small;
 
-	while (num++ <= 9) 
+	while (num++ <= 9)
 	{
 	for (small = 'a'; small <= 'z'; small++)
-	_putchar (small); 
+	_putchar (small);
 	_putchar ('\n');
 	}
 }
