b5f3932951a8d96675029f669f1a430eeee72baf
Anthony Olayemi <thonyfrancis8669@gmail.com>
2022-09-13 04:44:52 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,1 +1,15 @@
 #include "main.h"
+
+/**
+*main - Entry point
+*Description - Prints _putchar followed by a new line
+*
+*Return: Always (0)
+*/
+
+int main(void)
+{
+	write("_putchar/n");
+
+	return (0);
+}
