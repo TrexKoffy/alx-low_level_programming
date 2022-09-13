f8cef8097c4cf89dde71971a34858d7717eadd27
Anthony Olayemi <thonyfrancis8669@gmail.com>
2022-09-13 08:48:35 -0700

    me

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -10,7 +10,7 @@ void print_alphabet(void)
 {
 char small;
 
-for (small = 'a'; small <= 'z' ; small++)
+	for(small = 'a'; small <= 'z' ; small++)
 	_putchar (small);
 	_putchar ('\n');
 return (0);
