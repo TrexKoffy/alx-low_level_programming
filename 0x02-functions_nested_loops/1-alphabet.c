a23c97c02be31b95d59a8d04d3b56e19b7856d67
Anthony Olayemi <thonyfrancis8669@gmail.com>
2022-09-13 08:28:19 -0700

    message

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,1 +1,28 @@
 #include "main.h"
+
+/**
+*main - Prints all possible combinations of two two-digit numbers,
+*
+*Return: Always 0.
+*/
+
+int main(void)
+Task 3
+
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: alway returns 0
+ *
+ */
+int main(void)
+{
+
+char a;
+
+for (a = 'a'; a <= 'z' ; a++)
+	_putchar (a);
+	_putchar ('\n');
+return (0);
+}
