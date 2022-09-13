37b6c1fe1d49f18a44fda61b5158ca915c2ca4c5
Anthony Olayemi <thonyfrancis8669@gmail.com>
2022-09-13 09:33:12 -0700

    me

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,16 @@
 #include "main.h"
 
 /**
-*main - Prints the alpgabet jn lower case followed by a line.
+* print_alphabet  - Print in lower case followed by a line.
 *
-*Return: Always 0.
 */
 
-print_alphabet(void)
+void print_alphabet(void)
 {
 	char small;
 
 	for (small = 'a'; small <= 'z' ; small++)
 	_putchar (small);
 	_putchar ('\n');
-	return (0);
+
 }
