2147d63e2de379c0b811f53fbd2bbc986bfcdadd
Anthony Olayemi <thonyfrancis8669@gmail.com>
2022-10-11 01:02:05 +0000

    made corrections

--- 0x0D-preprocessor/1-pi.h
+++ 0x0D-preprocessor/1-pi.h
@@ -3,7 +3,7 @@
 
 /*
  * File: 1-pi.h
- * Desc: Header file that defines a macro named PI as an abbreviation for the token 3.14159265359.
+ * Desc: Header file that defines a macro named PI as an abbreviation.
  */
 
 #define PI 3.14159265359
